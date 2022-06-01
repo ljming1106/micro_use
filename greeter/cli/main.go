fa43b02f579e21d691e1fe07558c9d741ededa57
jl <jl@muchenglin.com>
2022-06-01 14:06:32 +0800

    wrapper,熔断,限流

--- greeter/cli/main.go
+++ greeter/cli/main.go
@@ -3,29 +3,60 @@ package main
 import (
 	"context"
 	"fmt"
+	"sync"
 
+	client "github.com/micro/go-micro/v2/client"
 	hello "github.com/micro/examples/greeter/srv/proto/hello"
 	"github.com/micro/go-micro/v2"
+
+	"github.com/micro/go-plugins/wrapper/breaker/hystrix/v2"
 )
 
 func main() {
 	// create a new service
-	service := micro.NewService()
+	service := micro.NewService(
+		// wrap the client
+		micro.WrapClient(logWrap),
+		micro.WrapClient(hystrix.NewClientWrapper()),
+		)
 
 	// parse command line flags
 	service.Init()
 
-	// Use the generated client stub
-	cl := hello.NewSayService("go.micro.srv.greeter", service.Client())
-
-	// Make request
-	rsp, err := cl.Hello(context.Background(), &hello.Request{
-		Name: "John",
-	})
-	if err != nil {
-		fmt.Println(err)
-		return
+	// concurrent request
+	wg := sync.WaitGroup{}
+	num := 10
+	wg.Add(num)
+	for i := 0;i<num;i++ {
+		go func() {
+			// Use the generated client stub
+			cl := hello.NewSayService("go.micro.srv.greeter1", service.Client())
+			// Make request
+			rsp, err := cl.Hello(context.Background(), &hello.Request{
+				Name: "John",
+			})
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			fmt.Println(rsp.Msg)
+			wg.Done()
+		}()
 	}
+	wg.Wait()
+}
 
-	fmt.Println(rsp.Msg)
+// log wrapper logs every time a request is made
+type logWrapper struct {
+	client.Client
+}
+
+func (l *logWrapper) Call(ctx context.Context, req client.Request, rsp interface{}, opts ...client.CallOption) error {
+	fmt.Printf("[wrapper] client request service: %s method: %s\n", req.Service(), req.Method())
+	return l.Client.Call(ctx, req, rsp)
+}
+
+// Implements client.Wrapper as logWrapper
+func logWrap(c client.Client) client.Client {
+	return &logWrapper{c}
 }
--- greeter/srv/main.go
+++ greeter/srv/main.go
@@ -3,9 +3,12 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"time"
 
+	limiter "github.com/micro/go-plugins/wrapper/ratelimiter/uber/v2"
 	hello "github.com/micro/examples/greeter/srv/proto/hello"
+	server "github.com/micro/go-micro/v2/server"
 	"github.com/micro/go-micro/v2"
 	"github.com/micro/go-micro/v2/util/log"
 	"google.golang.org/grpc"
@@ -14,10 +17,13 @@ import (
 type Say struct{}
 
 func (s *Say) Hello(ctx context.Context, req *hello.Request, rsp *hello.Response) error {
+	time.Sleep(500*time.Millisecond)
 	log.Log("Received Say.Hello request")
 	rsp.Msg = "Hello " + req.Name
 	return nil
 }
+
+const QPS = 5
 
 func main() {
 	go func() {
@@ -28,7 +34,10 @@ func main() {
 	}()
 
 	service := micro.NewService(
-		micro.Name("go.micro.srv.greeter"),
+		micro.Name("go.micro.srv.greeter1"),
+		// wrap the handler
+		micro.WrapHandler(logWrapper),
+		micro.WrapHandler(limiter.NewHandlerWrapper(QPS)),
 	)
 
 	// optionally setup command line usage
@@ -40,5 +49,14 @@ func main() {
 	// Run server
 	if err := service.Run(); err != nil {
 		log.Fatal(err)
+	}
+}
+
+// logWrapper is a handler wrapper
+func logWrapper(fn server.HandlerFunc) server.HandlerFunc {
+	return func(ctx context.Context, req server.Request, rsp interface{}) error {
+		fmt.Printf("[wrapper] server request: %v\n", req.Method())
+		err := fn(ctx, req, rsp)
+		return err
 	}
 }
